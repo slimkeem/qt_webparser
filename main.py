748364605df11af9d2440724a75f4729241327f7
Hakeem <aad018@mail.uit.com>
2022-03-23 08:57:31 +0100

    reworked the functions to work concurrently more smoothly

--- main.py
+++ main.py
@@ -3,6 +3,38 @@ from bs4 import BeautifulSoup
 import time
 
 
+def __extract_features(big_table_row, small_table_row):
+    article_id = big_table_row.get('id')
+
+    try:
+        author = small_table_row.find(class_='hnuser').string
+    except Exception as ex:
+        author = 'not_defined'
+
+    try:
+        points = small_table_row.find(class_='score').string
+    except Exception as ex:
+        points = '0 points'
+
+    comments_n = small_table_row.find_all(href=f'item?id={article_id}')[-1].string
+    if 'comments' in comments_n:
+        non_num_index = comments_n.find('comment')
+        comments_n = comments_n[:non_num_index].split()[0] + " comments"
+    else:
+        comments_n = '0 comments'
+
+    article_info = {
+        'Headline': big_table_row.find(class_='titlelink').string,
+        'Points': points,
+        'Author': author,
+        'Age': small_table_row.find(class_='age').string,
+        'Comments': comments_n,
+        'rank': big_table_row.span.string
+    }
+
+    return article_info
+
+
 def extract_features(page):
     headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/53.0.2785.143 Safari/537.36'}
@@ -14,36 +46,11 @@ def extract_features(page):
     if resp.status_code == 200:
         soup = BeautifulSoup(resp.content, 'html5lib')
         tr_blocks = soup.find_all('tr', class_='athing')
+        print(len(tr_blocks))
         smaller_tr_blocks = soup.find_all('td', class_='subtext')
 
         for index, tr in enumerate(tr_blocks):
-            article_id = tr.get('id')
-
-            try:
-                author = smaller_tr_blocks[index].find(class_='hnuser').string
-            except Exception as ex:
-                author = 'not_defined'
-
-            try:
-                points = soup.find(id=f'score_{article_id}').string
-                comments_n = smaller_tr_blocks[index].find_all(href=f'item?id={article_id}')[1].string
-                if comments_n != "discuss":
-                    non_num_index = comments_n.find('comment')
-                    comments_n = comments_n[:non_num_index].split()[0] + " comments"
-                else:
-                    comments_n = '0 comments'
-            except Exception as ex:
-                points = '0 points'
-
-            article_info = {
-                'Headline': tr.find(class_='titlelink').string,
-                'Points': points,
-                'Author': author,
-                'Age': smaller_tr_blocks[index].find(class_='age').string,
-                'Comments': comments_n,
-                'rank': tr.span.string
-            }
-            output_list.append(article_info)
+            output_list.append(__extract_features(tr, smaller_tr_blocks[index]))
 
         return output_list
 
@@ -53,8 +60,7 @@ if __name__ == '__main__':
     collated_output_list = []
 
     for page in required_pages:
-        output_list = extract_features(page)
-        collated_output_list += output_list
+        collated_output_list += extract_features(page)
         time.sleep(0.25)
 
     print(collated_output_list)
