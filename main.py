6e2eb6fd88cc7b812b1b773dfb4f7a395ed9f21d
Hakeem <aad018@mail.uit.com>
2022-03-23 02:28:39 +0100

    Inspecting and getting the required pages as links

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from bs4 import BeautifulSoup
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/53.0.2785.143 Safari/537.36'}
+    html = None
+    links = None
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    url = 'https://news.ycombinator.com/'
+
+    r = requests.get(url, headers=headers, timeout=10)
+
+    if r.status_code == 200:
+        html = r.text
+        soup = BeautifulSoup(html, 'html5lib')
+
+        available_pages = soup.select('a', href=True)
+        required_pages = ['news', 'ask', 'show']
+
+        links = [url + page['href'].strip() for page in available_pages if page['href'] in required_pages]
+
+        #     for page in available_pages:
+        #         if page["href"] in required_pages:
+        #             if 'http' not in page['href']:
+        #                 link = url+page["href"]
+        #             else:
+        #                 link = page["href"]
+        #             links.append(link.strip())
+        print(links)
