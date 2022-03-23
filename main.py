f2576abccd33e976bfa87ebace7fbec0ac85a9a2
Hakeem <aad018@mail.uit.com>
2022-03-23 06:52:14 +0100

    Made sequential loop for adding required items as dictionary into file

--- main.py
+++ main.py
@@ -1,31 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
 if __name__ == '__main__':
     headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/53.0.2785.143 Safari/537.36'}
-    html = None
-    links = None
+    BASE_URL = 'https://news.ycombinator.com/'
+    required_pages = ['news', 'ask', 'show']
+    output_list = []
 
-    url = 'https://news.ycombinator.com/'
+    for page in required_pages:
+        resp = requests.get(f'{BASE_URL}{page}', headers=headers, timeout=10)
 
-    r = requests.get(url, headers=headers, timeout=10)
+        if resp.status_code == 200:
+            soup = BeautifulSoup(resp.content, 'html5lib')
+            tr_blocks = soup.find_all('tr', class_='athing')
+            smaller_tr_blocks = soup.find_all('td', class_='subtext')
 
-    if r.status_code == 200:
-        html = r.text
-        soup = BeautifulSoup(html, 'html5lib')
+            for index, tr in enumerate(tr_blocks):
+                article_id = tr.get('id')
+                age = smaller_tr_blocks[index].find(class_='age').string
+                try:
+                    author = smaller_tr_blocks[index].find(class_='hnuser').string
+                except Exception as ex:
+                    author = 'not_defined'
 
-        available_pages = soup.select('a', href=True)
-        required_pages = ['news', 'ask', 'show']
-
-        links = [url + page['href'].strip() for page in available_pages if page['href'] in required_pages]
-
-        #     for page in available_pages:
-        #         if page["href"] in required_pages:
-        #             if 'http' not in page['href']:
-        #                 link = url+page["href"]
-        #             else:
-        #                 link = page["href"]
-        #             links.append(link.strip())
-        print(links)
+                try:
+                    points = soup.find(id=f'score_{article_id}').string
+                    comments_n = smaller_tr_blocks[index].find_all(href=f'item?id={article_id}')[1].string
+                    if comments_n != "discuss":
+                        non_num_index = comments_n.find('comment')
+                        comments_n = comments_n[:non_num_index].split()[0] + " comments"
+                    else:
+                        comments_n = '0 comments'
+                except Exception as ex:
+                    points = '0 points'
+                article_info = {
+                    'Headline': tr.find(class_='titlelink').string,
+                    'Points': points,
+                    'Author': author,
+                    'Age': age,
+                    'Comments': comments_n,
+                    'rank': tr.span.string
+                }
+                output_list.append(article_info)
+            time.sleep(0.5)
+    print(output_list[80])
